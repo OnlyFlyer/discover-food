e3f0718e080fc8f925ea4bd5d132024bc1763479
OnlyFlyer <scofield90.email@gmail.com>
2022-03-23 11:17:36 +0800

    feat: init

---
+++ lib/cq/index.ts
@@ -0,0 +1,29 @@
+/*
+ * @Author: OnlyFlyer
+ * @Date: 2022-03-23 11:14:03 
+ * @Last Modified by: OnlyFlyer
+ * @Last Modified time: 2022-03-23 11:17:22
+ */
+
+export const addrs = [
+  {
+    name: '何姐餐厅',
+    addr: '',
+  },
+  {
+    name: '醉八仙',
+  },
+  {
+    name: '四季羊肉馆',
+  },
+  {
+    name: '四季羊肉馆',
+  },
+  {
+    name: '玖玖蒸菜',
+  },
+  {
+    name: '玖玖蒸菜',
+  },
+  {}
+]
